479e1e24a944659557a60db2a4b3a902d71f8f9e
PTAHume <patricktahume@gmail.com>
2022-06-14 16:51:22 +0100

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { StrictMode, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+function Button(props: any) {
+  const handelClick = () => {
+    props.onClickFunction(props.increment);
+  };
+  return <button onClick={handelClick}>+{props.increment}</button>;
+}
+
+function Display(props: any) {
+  return <div>{props.messsage}</div>;
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => {
+    setCounter(counter + incrementValue);
+  };
+  return (
+    <>
+      <Button onClickFunction={incrementCounter} increment={1} />
+      <Button onClickFunction={incrementCounter} increment={2} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Display messsage={counter} />
+    </>
+  );
+}
+
+root.render(<App />);
